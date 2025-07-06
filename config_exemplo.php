f9405c30adc53482aefbaecd25798fc68f6811f5
Maria Eduarda Batista Henrique <145713858+MariaEduardaBatt@users.noreply.github.com>
2025-07-06 11:09:34 -0300

    Update and rename config.php to config_exemplo.php

--- config_exemplo.php
+++ config_exemplo.php
@@ -1,9 +1,9 @@
 <?php
 // Configurações do banco de dados
-$host = 'sql313.infinityfree.com';
-$database = 'if0_39406156_db_crud';
-$username = 'if0_39406156';
-$password = 'senhaInfinity22'; 
+$host = 'nomeHost'; // Substitua pelo nome do host do seu banco de dados
+$database = 'db_database'; // Substitua pelo nome do seu banco de dados
+$username = 'username'; // Substitua pelo nome de usuário do seu banco de dados
+$password = 'senha';   // Substitua pela senha do seu banco de dados 
 
 // Conexão com o banco de dados
 $conexao = new mysqli($host, $username, $password, $database);
